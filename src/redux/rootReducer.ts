7a48c608cefd04e41b0667e09606b2f845cc4119
naofalleoagusta <naofal@ekrut.com>
2021-11-20 14:35:08 +0700

    added movie detail

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,10 +2,8 @@ import { combineReducers } from "redux";
 import movieList from "./movieList/reducer";
 import optionList from "./options/reducer";
 import movieDetail from "./moviesDetail/reducer";
-import apiReducer from "./api/";
 
 const appReducer = combineReducers({
-  app: apiReducer,
   movieList: movieList,
   options: optionList,
   movieDetail: movieDetail,
